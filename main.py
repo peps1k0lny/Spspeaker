6659dcc32ba539d7e0e66b1628b262605fd8f023
aleksandrstrelcenko <alexstrelchen2006@gmail.com>
2024-03-12 22:24:57 +0300

    added multiple playlists

--- main.py
+++ main.py
@@ -22,29 +22,49 @@ class Spspeaker:
         self.pause = False
 
         self.btnpause = Button(23)
-        self.btnpllist1 = Button(24)
+        self.btnpllist1 = Button(26)
+        self.btnpllist2 = Button(19)
+        self.btnpllist3 = Button(13)
+        self.btnpllist4 = Button(6)
+        self.btnpllist5 = Button(5)
 
         while True:
             self.btnpause.when_pressed = self.pause_or_start
-            self.btnpllist1.when_pressed = self.start_playlist
+            self.btnpllist1.when_pressed = lambda: [self.start_playlist(0)]
+            self.btnpllist2.when_pressed = lambda: [self.start_playlist(1)]
+            self.btnpllist3.when_pressed = lambda: [self.start_playlist(2)]
+            self.btnpllist4.when_pressed = lambda: [self.start_playlist(3)]
+            self.btnpllist5.when_pressed = lambda: [self.start_playlist(4)]
 
     def pause_or_start(self):
+        self.sp.transfer_playback(device_id=self.DEVICE_ID, force_play=False)
         if self.pause is False:
             print('start')
-            self.sp.start_playback()
+            self.sp.start_playback(device_id=self.DEVICE_ID)
             self.pause = True
         elif self.pause is True:
             print('pause')
-            self.sp.pause_playback()
+            self.sp.pause_playback(device_id=self.DEVICE_ID)
             self.pause = False
 
-    def start_playlist(self):
+    def start_playlist(self, num):
+        playlists = ["spotify:playlist:4E4pze60aCpMHbslYqIyQx",
+                     "spotify:playlist:37i9dQZF1EIhkGftn1D0Mh",
+                     "spotify:playlist:37i9dQZF1Fa1IIVtEpGUcU",
+                     "spotify:playlist:37i9dQZF1EIeYLhx9SJ9SI",
+                     "spotify:playlist:337o1CcuOoRfoTbmpZ4enF"]
         self.pause = True
-        print('aboba')
+        print('playlist', num + 1)
         self.sp.transfer_playback(device_id=self.DEVICE_ID, force_play=False)
         self.sp.start_playback(device_id=self.DEVICE_ID,
-                               context_uri="spotify:playlist:4E4pze60aCpMHbslYqIyQx",
-                               offset={"position": 0})
+                               context_uri=playlists[num],
+                               offset={"position": 0},
+                               position_ms=0)
+
+    def next_track(self):
+        self.pause = True
+        self.sp.transfer_playback(device_id=self.DEVICE_ID, force_play=False)
+        self.sp.next_track(device_id=self.DEVICE_ID)
 
 
 if __name__ == '__main__':
