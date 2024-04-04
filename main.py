b141b5618e5ce2479753507ec0378225abf9110e
aleksandrstrelcenko <alexstrelchen2006@gmail.com>
2024-04-04 18:27:46 +0300

    added sound control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from gpiozero import Button
+from gpiozero import Button, RotaryEncoder
 
 
 class Spspeaker:
@@ -30,12 +30,24 @@ class Spspeaker:
         self.button_like = Button(24)
 
         self.button_playlist1 = Button(26)
-        self.button_playlist2 = Button(19)
+        self.button_playlist2 = Button(15)
         self.button_playlist3 = Button(13)
         self.button_playlist4 = Button(6)
         self.button_playlist5 = Button(5)
 
+        self.rotor = RotaryEncoder(7, 8, wrap=True, max_steps=100)
+        self.button_mute = Button(25)
+
+        self.ismute = False
+        self.rotor.steps = (self.sp.current_playback()["device"]["volume_percent"] -
+                            self.sp.current_playback()["device"]["volume_percent"] % 5)
+
         while True:
+            if self.ismute is False:
+                self.rotor.when_rotated_clockwise = self.plus_volume
+                self.rotor.when_rotated_counter_clockwise = self.minus_volume
+            self.button_mute.when_pressed = self.mute
+
             self.button_pause.when_pressed = self.pause_or_start
             self.button_next.when_pressed = self.next_track
             self.button_prev.when_pressed = self.prev_track
@@ -93,6 +105,26 @@ class Spspeaker:
         print("like")
         self.sp.current_user_saved_tracks_add([self.sp.current_user_playing_track()["item"]["uri"]])
         print(self.sp.current_user_playing_track()["item"]["uri"])
+
+    def plus_volume(self):
+        if self.rotor.steps < 100:
+            self.rotor.steps += 4
+            print(self.rotor.steps)
+            self.sp.volume(self.rotor.steps, device_id=self.DEVICE_ID)
+
+    def minus_volume(self):
+        if self.rotor.steps > 0:
+            self.rotor.steps -= 4
+            print(self.rotor.steps)
+            self.sp.volume(self.rotor.steps, device_id=self.DEVICE_ID)
+
+    def mute(self):
+        if self.ismute is False:
+            self.ismute = True
+            self.sp.volume(0, device_id=self.DEVICE_ID)
+        else:
+            self.sp.volume(self.rotor.steps, device_id=self.DEVICE_ID)
+            self.ismute = False
 
 
 if __name__ == '__main__':
